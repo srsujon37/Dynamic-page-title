fa0edb38f4a32446fc4bffb615a710bf36422780
srsujon37 <srsujon97@gmail.com>
2025-04-26 11:35:08 +0600

    ddd

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { APIProvider } from '@vis.gl/react-google-maps'
+import { googleMapAPIkey } from './map-api-key.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <APIProvider apiKey={googleMapAPIkey}>
     <App />
+    </APIProvider>
   </StrictMode>,
 )
